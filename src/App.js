6609f2ccbfb731246e2b7d2c93035daf0b2a6036
Joshua Lehman <joshlehman.dev@gmail.com>
2023-01-14 22:44:08 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import TargetElementsWithRef from "./components/2_TargetingElementsWithRefs";
 import ScopedSelectors from "./components/3_ScopedSelectors";
 import ReusingComponents from "./components/4_Reusing_Components";
 import CreatingControllingTimelines from "./components/5_CreatingControllingTimelines";
+import ControllingWhenReactCreatesAnimation from "./components/6_ControllingWhenReactCreatesAnimation";
+import ReactingToChangeInState from "./components/7_ReactingToChangesInState";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React GSAP Examples</h1>
-      <div className="spacer" />
-      <AnimatingOnInteraction />
-      <div className="spacer" />
+    <section>
+      <div>
+        <h1>React GSAP Examples</h1>
+        <div className="spacer" />
+      </div>
+      <div>
+        <AnimatingOnInteraction />
+        <div className="spacer" />
+      </div>
       <TargetElementsWithRef />
       <div className="spacer" />
       <ScopedSelectors />
@@ -20,7 +26,11 @@ function App() {
       <ReusingComponents />
       <div className="spacer" />
       <CreatingControllingTimelines />
-    </div>
+      <div className="spacer" />
+      <ControllingWhenReactCreatesAnimation />
+      <div className="spacer" />
+      <ReactingToChangeInState />
+    </section>
   );
 }
 
